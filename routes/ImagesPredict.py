18a289d97263dc860238d59f0c9e338eb27b66e8
Verónica Guzmán Ochoa <vero.guzmanochoa@gmail.com>
2024-05-21 08:22:32 -0700

    Added HTTP protocol alternative

--- routes/ImagesPredict.py
+++ routes/ImagesPredict.py
@@ -1,7 +1,6 @@
-from fastapi import APIRouter, WebSocket
+from fastapi import APIRouter, Body, HTTPException
 from io import BytesIO
 from PIL import Image
-import json
 import base64
 from ultralytics import YOLO
 from models import Prediction, Object
@@ -9,40 +8,71 @@ import torch
 
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 print(f'Using device: {device}')
-
 model = YOLO("gun_detection_model.pt").to(device)
 
 router = APIRouter()
 
+# Example
+# ip:port/objects-list -> returns
+# [
+#   {
+#       "object_name": Objeto 1"
+#   },
+#   {
+#       "object_name": Objeto 2"
+#   },
+#   {
+#       "object_name": Objeto 3"
+#   },
+# ]
+@router.get("/objects-list")
+def predict(base64_image: str = Body(...)):
+    # Cut the data url metadata and encode it in ascii
+    base64_image = base64_image.split(",")[1]
+    base64_bytes = base64_image.encode(encoding="ascii")
 
-@router.websocket("/predict")
-async def predict(websocket: WebSocket):
-    await websocket.accept();
+    # Decode the base64 image
+    decoded_image = BytesIO(base64.b64decode(base64_bytes))
 
-    while True:
-        base64_image = await websocket.receive_text()
-        # Cut the data url metadata and encode it in ascii
-        base64_image = base64_image.split(",")[1]
-        base64_bytes = base64_image.encode(encoding="ascii")
+    # Make an inference
+    source = Image.open(decoded_image)
+    width, height = source.size
 
-        # Decode the base64 image
-        decoded_image = BytesIO(base64.b64decode(base64_bytes))
+    results = model(source=source, imgsz=[640, 480], rect=False)
 
-        # Make an inference
-        source = Image.open(decoded_image)
-        results = model(source=source)
-        
-        if results[0].__len__() > 0:
+    # Response is a list of predictions 
+    response = []
+
+    if results[0].__len__() > 0:
+        try:
             for result in results:
+
                 boxes = result.boxes.cpu().numpy()
+
                 for box in boxes:
-                    # Create a response based on the prediction response model
-                    coordinates = box.xyxy[0].tolist()
-                    inferenced_object = box.cls[0].tolist()
-                    probability = box.conf[0].tolist()
-                    inferenced_object = model.names[inferenced_object]
-                    prediction = Prediction(x_coordinate=coordinates[0], y_coordinate=coordinates[1], x2_coordinate=coordinates[2], y2_coordinate=coordinates[3], detected_object=inferenced_object, probs=probability)
-                    await websocket.send_json(prediction.model_dump())                    
+                    if box.conf[0].tolist()[0] >= 60:
+                        # Create a response based on the prediction response model
+                        prediction = Prediction()
+                        coordinates = box.xyxy[0].tolist()
+                        inferenced_object = box.cls[0].tolist()
+                        probability = box.conf[0].tolist()
+                        inferenced_object = model.names[inferenced_object]
+
+                        prediction.x_coordinate = coordinates[0]
+                        prediction.y_coordinate = coordinates[1]
+                        prediction.x2_coordinate = coordinates[2]
+                        prediction.y2_coordinate = coordinates[3]
+                        prediction.detected_object = inferenced_object
+                        prediction.probs = probability
+
+                        response.append(prediction)
+        except Exception as e:
+            raise HTTPException(500, str(e))
+    else:
+        raise HTTPException(404, "No inference")
+
+    return response
+
 # Example
 # ip:port/objects-list -> returns
 # [
